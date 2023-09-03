44361e03cc88f749bf4b50cb6e3814dbf8f1d0bc
sozzifer <sozzifer@gmail.com>
2023-09-03 22:15:49 +0100

    `User.followed`

--- app/models.py
+++ app/models.py
@@ -35,24 +35,24 @@ class User(UserMixin, db.Model):
     posts: RelationshipProperty = db.relationship(
         "Post", backref="author", lazy="dynamic"
     )
-    """The `followed` RelationshipProperty links a `User` instance to other `User` instances. For two `User`s linked by this relationship, the left-hand `User` follows the right-hand `User`, i.e. the relationship is defined as seen from the left-hand `User`. Querying on the left-hand side returns the list of `User`s followed by `self.User`.
+    """The `followed` RelationshipProperty links an instance of `User` to other `User` instances. For two users linked by this relationship, the left side `follower` user follows the right side `followed` user, i.e. the relationship is defined as seen from the left side user. Querying on the `follower` user returns a list of `followed` users.
     """
     followed: RelationshipProperty = db.relationship(
-        # "User" is the class to be used on the right-hand side of the relationship. The left-hand side is the parent `User` class. Since this is a self-referential relationship, the same class is used on both sides.
+        # "User" is the class to be used on the right side of the relationship. The left side is the parent `User` class. Since this is a self-referential relationship, the same class is used on both sides.
         "User",
         # `secondary` sets `followers` as the association table to be used for this relationship.
         secondary=followers,
-        # `primaryjoin` indicates the condition that links the left side entity (the `follower` user) with the association table. The join condition for the left side of the relationship is the user ID matching the `follower_id` field in the association table.
+        # `primaryjoin` indicates the condition that links the left side entity (the `follower` user) with the association table. The joining condition for the left side of the relationship is the user ID matching the `follower_id` field in the association table.
         primaryjoin=(followers.c.follower_id == id),
         # `secondaryjoin` indicates the condition that links the right side entity (the `followed` user) with the association table. The join condition for the right side of the relationship is the user ID matching the `followed_id` field in the association table.
         secondaryjoin=(followers.c.followed_id == id),
-        # `backref` defines how this relationship will be accessed from the right side entity. From the left side, the relationship is `followed`, so from the right side `followers` represents all the left side users that are linked to the target right side user. `lazy`="dynamic" sets the right side query to run only if specifically requested.
+        # `backref` defines how this relationship will be accessed from the right side entity. From the left side, the relationship is `followed`, so from the right side `followers` represents all the left side users that are linked to the target right side user.
         backref=db.backref("followers", lazy="dynamic"),
-        # `lazy`="dynamic" sets the left side query to run only if specifically requested.
+        # `lazy`="dynamic" sets the left side query to run only if specifically requested (see also `backref`).
         lazy="dynamic",
     )
 
-    """sqlalchemy.exc.ArgumentError: Type annotation for "User.posts" can't be correctly interpreted for Annotated Declarative Table form.  ORM annotations should normally make use of the ``Mapped[]`` generic type, or other ORM-compatible generic type, as a container for the actual type, which indicates the intent that the attribute is mapped. Class variables that are not intended to be mapped by the ORM should use ClassVar[].  To allow Annotated Declarative to disregard legacy annotations which don't use Mapped[] to pass, set "__allow_unmapped__ = True" on the class or a superclass this class. (Background on this error at: https://sqlalche.me/e/20/zlpr)
+    """sqlalchemy.exc.ArgumentError: Type annotation for "User.posts" can't be correctly interpreted for Annotated Declarative Table form.  ORM annotations should normally make use of the ``Mapped[]`` generic type, or other ORM-compatible generic type, as a container for the actual type, which indicates the intent that the attribute is mapped. Class variables that are not intended to be mapped by the ORM should use ClassVar[].  To allow Annotated Declarative to disregard legacy annotations which don't use Mapped[] to pass, set "__allow_unmapped__ = True" on the class or a superclass of this class. (Background on this error at: https://sqlalche.me/e/20/zlpr)
     """
     __allow_unmapped__ = True
 
